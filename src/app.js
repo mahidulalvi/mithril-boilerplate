5f44f6ac6ee0739c735f0a339ccf887fced8109c
Md Mahidul Haque Alvi <mdmahidulhaque001@gmail.com>
2020-10-04 17:41:50 -0500

    Added 'mithril' support in the project.
    Also created two example components for better understanding mithril functionalities.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
-const util = require('./util');
-import message from './message';
-console.log(util.formatUpperCase(message));
+import m from 'mithril';
+// import ExampleMithrilComponent from './components/ExampleMithrilComponent/ExampleMithrilComponent.js';
+import ExampleMithrilComponent2 from './components/ExampleMithrilComponent/ExampleMithrilComponent2.js';
+
+let globalCount = 0;
+const increaseGlobalCount = () => {
+  globalCount += 1;
+};
+
+// m.mount(document.body, ExampleMithrilComponent);
+
+const rerender = () => {
+  m.render(
+    document.body,
+    m(ExampleMithrilComponent2, {
+      globalCount: globalCount,
+      increaseGlobalCount: increaseGlobalCount,
+    }),
+    () => rerender()
+  );
+};
+
+m.render(
+  document.body,
+  m(ExampleMithrilComponent2, {
+    globalCount: globalCount,
+    increaseGlobalCount: increaseGlobalCount,
+  }),
+  () => rerender()
+);
