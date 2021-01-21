aa871a3407407bca535e900256c68b166cb0e882
Md Mahidul Haque Alvi <mdmahidulhaque001@gmail.com>
2021-01-20 20:50:32 -0600

    Enhanced the Redux Mithril binding 'connect' function through Route Resolvers.
    In the previous, the store was subscribed inside the 'connect' function. It was not ideal to implement other possible Route Resolver functionalities (e.g. an application layout component) as the 'connect' function was returning a Route Resolver to deliver updated vnode attrs to the component on store update. So a new function 'RouteResolver' was created to return a Route Resolver object. This function can handle connected & non connected components as params and returns the components along with its vnode attrs through a Route Resolver object.
    
    The connect function now solely handles getting latest props from the store & is not concerned with store subscriptions. The store is subscribed in the Redux config file (src/reduxConfig/config.js) and m.redraw() is called in the subscription callback. A more efficient way of redrawing rather than calling it on every dispatch call may be implemented later.

--- src/reduxConfig/config.js
+++ src/reduxConfig/config.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers } from 'redux';
 import { exampleMithrilComponentReducer } from '../components/ExampleMithrilComponent/reducer.js';
 import { i18nReducer } from '../i18nConfig/reducer.js';
+import m from 'mithril';
 
 const rootReducer = combineReducers({
   exampleComponent: exampleMithrilComponentReducer,
@@ -11,5 +12,16 @@ const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+/**
+ * updating the DOM when an action is dispatched to the Redux store.
+ * this will cause components to rerender with latest props from store.
+ * see the src/RouteResolver.js and src/reduxConfig/connect.js files for
+ * more details on how the component gets the latest props.
+ *
+ * a more efficient method for redrawing on store update might be
+ * implemented later.
+ */
+store.subscribe(() => m.redraw());
 
 export { store };
